0184da1e9ce68db29c0eecfed7a4170067643334
saviruperera <saviru.perera@mail.mcgill.ca>
2025-02-07 18:14:31 -0500

    more backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,1 +1,12 @@
-import express from "express";
+import app from "./app.js";
+import { connectToDatabase } from "./db/db.js";
+
+const port = process.env.PORT || 5002;
+
+connectToDatabase()
+    .then(() => {
+        app.listen(port, () => console.log(`Server is running on port ${port} and connected to the database`));
+    })
+    .catch((err) => {
+        console.error(err);
+    });
