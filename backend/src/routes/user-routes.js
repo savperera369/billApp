d8237c3cb75aa5ca23a36a5ad7d361d9db724247
savperera369 <saviru2002@outlook.com>
2025-02-20 15:30:08 -0500

    implement logout functionality

--- backend/src/routes/user-routes.js
+++ backend/src/routes/user-routes.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
-import { userLogin, userSignUp } from "../controllers/user-controllers.js";
+import { userLogin, userSignUp, userLogout } from "../controllers/user-controllers.js";
 import { loginValidator, signUpValidator, validate } from "../utils/validators.js";
+import { verifyToken } from "../utils/token-manager.js";
 
 const userRoutes = Router();
 
 userRoutes.post("/signup", validate(signUpValidator), userSignUp);
 userRoutes.post("/login", validate(loginValidator), userLogin);
+userRoutes.get("/logout", verifyToken, userLogout);
 
 export default userRoutes;
