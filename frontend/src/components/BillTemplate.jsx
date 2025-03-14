577b0271ecd79d6dc1bd188bac400e0d61b4cdf8
saviruperera <saviru.perera@mail.mcgill.ca>
2025-03-13 23:19:29 -0400

    small style updates

--- frontend/src/components/BillTemplate.jsx
+++ frontend/src/components/BillTemplate.jsx
@@ -1,19 +1,21 @@
 import { SlLike } from "react-icons/sl";
 import { SlDislike } from "react-icons/sl";
+import { BsSave } from "react-icons/bs";
 
 const BillTemplate = () => {
     return (
-        <div className="bg-gray-100 rounded-md p-4 shadow-md flex flex-col gap-y-4 w-full">
+        <div className="bg-gray-100 rounded-xl p-4 shadow-md flex flex-col gap-y-4 w-full hover:shadow-2xl">
             <p className="text-xl text-slate-500 font-bold text-center">Bill Title</p>
-            <p className="text-sm text-slate-500">
+            <p className="text-md text-slate-500 text-center">
                 This bill is about the recent changes in tariffs in Canada and Mexico.
                 As things stand we are pausing the imposing of these massive tariffs on
                 our Trans-NAFTA counterparts as we fear this may negatively impact our relation
                 with these countries.
             </p>
-            <div className="flex items-center gap-x-4">
+            <div className="flex items-center gap-x-4 px-8">
                 <SlLike className="h-4 w-4" />
                 <SlDislike className="h-4 w-4" />
+                <BsSave className="h-4 w-4" />
             </div>
         </div>
     );
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,8 +2,8 @@ import BillTemplate from "../components/BillTemplate.jsx";
 
 const Home = () => {
     return (
-        <div className="flex flex-col items-center gap-y-4 w-1/2 overflow-scroll mx-auto">
-            <p className="text-3xl font-bold text-slate-500">Popular Bills</p>
+        <div className="flex flex-col items-center gap-y-8 w-2/3 mx-auto">
+            <p className="text-3xl font-bold text-slate-500 mb-4">Popular Bills</p>
             <BillTemplate />
             <BillTemplate />
             <BillTemplate />
